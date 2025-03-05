c53294147f2a59d9cc1e82e811e4373c875f9514
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2025-03-05 01:45:14 +0100

    fix: improve Redis readliness check in CI

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import redis
 import os
 
 # Pobranie adresu hosta Redis z zmiennej środowiskowej
-redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
 
 # Łączenie z Redisem
 r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
