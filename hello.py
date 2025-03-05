be04060efb4e753220c0956cd2ac89e6ccc23855
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2025-03-05 01:32:24 +0100

    fix: use REDIS_HOST env variable for better compatibility

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 import redis
+import os
+
+# Pobranie adresu hosta Redis z zmiennej środowiskowej
+redis_host = os.getenv("REDIS_HOST", "localhost")
 
 # Łączenie z Redisem
-r = redis.Redis(host='redis', port=6379, decode_responses=True)
+r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 # Zwiększ licznik uruchomień
 r.incr('counter')
