330c1818dbb39e38360eadad3694c750b126c642
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2025-03-06 22:30:16 +0100

    feat: add Kubernetes deployment and service configuration, and update hello.py with loop

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import redis
 import os
+import time
 
 # Pobranie adresu hosta Redis z zmiennej środowiskowej
 redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
@@ -7,10 +8,14 @@ redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
 # Łączenie z Redisem
 r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
-# Zwiększ licznik uruchomień
-r.incr('counter')
+while True:
 
-# Pobierz wartość countera
-count = r.get('counter')
+	# Zwiększ licznik uruchomień
+	r.incr('counter')
 
-print(f"Hello World! This script has been run {count} times")
+	# Pobierz wartość countera
+	count = r.get('counter')
+
+	print(f"Hello World! This script has been run {count} times")
+
+	time.sleep(5)
